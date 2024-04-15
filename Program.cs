06733ad0fee1c97b6d326b7586e2c77bfee616fa
Laxman Rajbhar <laxman.rajbah@gmail.com>
2024-04-15 11:45:58 +0530

    ADded project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int s = 45;
+        Console.WriteLine($"{s:C2}"); 
+               
+        Console.ReadLine();
+
+
+
+    }
+}
